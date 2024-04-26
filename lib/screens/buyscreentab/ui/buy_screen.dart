eab8d5907c68f5900f8f1c1c6573439281e8902a
hijas <hijasahamed2000@gmail.com>
2024-04-26 22:49:56 +0530

    latest push

--- lib/screens/buyscreentab/ui/buy_screen.dart
+++ lib/screens/buyscreentab/ui/buy_screen.dart
@@ -21,54 +21,47 @@ class BuyScreen extends StatelessWidget {
           '9,563Km',
           '₹1.52 Cr'),
       AllCars(
-        'https://images.carandbike.com/cms/articles/2023/3/3206096/2023_Honda_City_Facelift_IVTEC_Static_2_ae3a3bc71c.jpg',
-        'Honda City',
-        '2024 Model',
-        '5,136Km',
-        '₹21.82 Lakh'
-      ),
+          'https://images.carandbike.com/cms/articles/2023/3/3206096/2023_Honda_City_Facelift_IVTEC_Static_2_ae3a3bc71c.jpg',
+          'Honda City',
+          '2024 Model',
+          '5,136Km',
+          '₹21.82 Lakh'),
       AllCars(
-        "https://stimg.cardekho.com/images/carexteriorimages/930x620/Skoda/Superb-2024/11648/1712204642647/front-left-side-47.jpg",
-        'Skoda Superb',
-        '2019 Model',
-        '24,320Km',
-        '₹27.21 Lakh'
-      ),
+          "https://stimg.cardekho.com/images/carexteriorimages/930x620/Skoda/Superb-2024/11648/1712204642647/front-left-side-47.jpg",
+          'Skoda Superb',
+          '2019 Model',
+          '24,320Km',
+          '₹27.21 Lakh'),
       AllCars(
-        "https://www.financialexpress.com/wp-content/uploads/2023/03/2023-Hyundai-Verna-Review-1.jpg",
-        'Hyundai Verna',
-        '2023 Model',
-        '2,310Km',
-        '₹17.82 Lakh'
-      ),
+          "https://www.financialexpress.com/wp-content/uploads/2023/03/2023-Hyundai-Verna-Review-1.jpg",
+          'Hyundai Verna',
+          '2023 Model',
+          '2,310Km',
+          '₹17.82 Lakh'),
       AllCars(
-        'https://stimg.cardekho.com/images/carexteriorimages/930x620/Maruti/Grand-Vitara/10505/1689588262879/front-left-side-47.jpg',
-        'Grand Vitara',
-        '2023 Model',
-        '9556Km',
-        '₹15.65 Lakh'
-      ),
+          'https://stimg.cardekho.com/images/carexteriorimages/930x620/Maruti/Grand-Vitara/10505/1689588262879/front-left-side-47.jpg',
+          'Grand Vitara',
+          '2023 Model',
+          '9556Km',
+          '₹15.65 Lakh'),
       AllCars(
-        'https://spn-sta.spinny.com/blog/20220308152631/VW-Virtus-launch.jpg?compress=true&quality=80&w=600&dpr=2.6',
-        'VW-Virtus',
-        '2021 Model',
-        '43,310Km',
-        '₹13.82 Lakh'
-      ),
+          'https://spn-sta.spinny.com/blog/20220308152631/VW-Virtus-launch.jpg?compress=true&quality=80&w=600&dpr=2.6',
+          'VW-Virtus',
+          '2021 Model',
+          '43,310Km',
+          '₹13.82 Lakh'),
       AllCars(
-        'https://www.globalsuzuki.com/automobile/lineup/ignis/img/slide/key_img11.jpg',
-        'Ignis',
-        '2018 Model',
-        '38,896Km',
-        '₹7.8 Lakh'
-      ),
+          'https://www.globalsuzuki.com/automobile/lineup/ignis/img/slide/key_img11.jpg',
+          'Ignis',
+          '2018 Model',
+          '38,896Km',
+          '₹7.8 Lakh'),
       AllCars(
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/158139/i20-n-line-exterior-left-front-three-quarter.jpeg?isig=0&q=80',
-        'i20 Nline',
-        '2023 Model',
-        '29,310Km',
-        '₹8.82 Lakh'
-      ),
+          'https://imgd.aeplcdn.com/664x374/n/cw/ec/158139/i20-n-line-exterior-left-front-three-quarter.jpeg?isig=0&q=80',
+          'i20 Nline',
+          '2023 Model',
+          '29,310Km',
+          '₹8.82 Lakh'),
     ];
     return Scaffold(
         body: Padding(
@@ -93,19 +86,35 @@ class BuyScreen extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Container(
-                            height: screenSize.height / 7.5,
-                            width: screenSize.width,
-                            decoration: BoxDecoration(
-                                borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(5),
-                                    topRight: Radius.circular(4)),
-                                color: Colors.yellow,
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                        carDetails[index].imageurl),
-                                    fit: BoxFit.cover,
-                                    filterQuality: FilterQuality.high)),
+                          Stack(
+                            children: [
+                              Container(
+                                height: screenSize.height / 7.5,
+                                width: screenSize.width,
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(5),
+                                        topRight: Radius.circular(4)),
+                                    color: Colors.transparent,
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            carDetails[index].imageurl),
+                                        fit: BoxFit.cover,
+                                        filterQuality: FilterQuality.high)),
+                              ),
+                              Positioned(
+                                top: 5,
+                                right: 5,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  radius: 15,
+                                  child: IconButton(
+                                    onPressed: (){}, 
+                                    icon: const Icon(Icons.favorite_outline_rounded,size: 15,color: Colors.red,)
+                                  ),
+                                )
+                              )
+                            ],
                           ),
                           Padding(
                             padding: const EdgeInsets.only(left: 4),
@@ -190,7 +199,7 @@ class BuyScreen extends StatelessWidget {
                       ],
                     ),
                   ),
-                )
+                ),
               ],
             )));
   }
