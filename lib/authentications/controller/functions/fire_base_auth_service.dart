81abcf75149e0e06469b2c29ea1975a1a6cb5325
hijas <hijasahamed2000@gmail.com>
2024-05-03 19:12:30 +0530

    latest push

--- lib/authentications/controller/functions/fire_base_auth_service.dart
+++ lib/authentications/controller/functions/fire_base_auth_service.dart
@@ -7,8 +7,6 @@ class FirebaseAuthService {
   FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<User?> userLogin(String email, String password) async {
-    print(email);
-    print(password);
     try {
       UserCredential credential = await auth.signInWithEmailAndPassword(
           email: email, password: password);
@@ -22,16 +20,13 @@ class FirebaseAuthService {
   }
 
   Future<User?> userSignup(String email, String password) async {
-    print('auth function working');
-    print(email);
-    print(password);
     try {
       UserCredential credential = await auth.createUserWithEmailAndPassword(
           email: email, password: password);
       return credential.user;
     } catch (e) {
       if (kDebugMode) {
-        print('e print $e');
+        print(e);
       }
     }
     return null;
