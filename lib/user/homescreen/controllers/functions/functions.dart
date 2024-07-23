e32022723a10c24b7437ff6582976d02e34fee69
hijas <hijasahamed2000@gmail.com>
2024-07-23 22:00:48 +0530

    latest push

--- lib/user/homescreen/controllers/functions/functions.dart
+++ lib/user/homescreen/controllers/functions/functions.dart
@@ -7,3 +7,28 @@ Stream<QuerySnapshot> getfavouriteCars({String? userContact}){
   .where('userContact',isEqualTo: userContact)
   .snapshots();
 }
+
+Stream<List<Map<String, dynamic>>> findTopRatedSellers() async* {
+  final CollectionReference collection = FirebaseFirestore.instance.collection('sellerSignupData');
+
+  yield* collection.snapshots().map((snapshot) {
+    List<Map<String, dynamic>> sellers = [];
+
+    for (var doc in snapshot.docs) {
+      final data = doc.data() as Map<String, dynamic>;
+      List<dynamic> ratings = data['rating'] ?? [];
+
+      double sum = ratings.fold(0.0, (previousValue, element) => previousValue + (element is num ? element.toDouble() : 0.0));
+
+      sellers.add({
+        'id': doc.id,
+        'data': data,
+        'sumRating': sum,
+      });
+    }
+
+    sellers.sort((a, b) => b['sumRating'].compareTo(a['sumRating']));
+
+    return sellers;
+  });
+}
