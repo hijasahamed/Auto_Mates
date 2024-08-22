6dd0955f7a60f1902be2fac979ba93ab021bd080
hijas <hijasahamed2000@gmail.com>
2024-08-22 19:30:58 +0530

    latest update push

--- lib/user/splashscreen/view/splash_screen/splash_screen.dart
+++ lib/user/splashscreen/view/splash_screen/splash_screen.dart
@@ -1,7 +1,6 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:auto_mates/user/commonwidgets/my_text_widget/my_text_widget.dart';
 import 'package:auto_mates/user/splashscreen/controllers/functions.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -22,30 +21,20 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context){
     final screenSize =MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: const Color(0xFFDBEDF5), 
       body: SizedBox(
         height: screenSize.height,
         width: screenSize.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            LottieBuilder.asset('assets/animations/splash_animation_jeep.json',repeat: false,), 
-            AnimatedTextKit(
-              animatedTexts: [
-                TypewriterAnimatedText(
-                  'Auto Mates',
-                  textStyle: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  speed: const Duration(milliseconds: 220),
-                  cursor: ''
-                ),
-              ],
-              isRepeatingAnimation: false,
-              repeatForever: false,              
+            Container(
+              height: screenSize.height/7,
+              width: screenSize.width/3,
+              decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/images/ic_launcher.png',),fit: BoxFit.cover)
+              ),
             ),
+            MyTextWidget(text: 'Auto Mates', color: Colors.blueGrey, size: screenSize.width/20, weight: FontWeight.bold)
           ],
         ),
       ),
